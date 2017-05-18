9e6c015472d484ce5379d8436857c84d82ef6808
Jaime Pagan <jimbopagan@yahoo.com>
2017-05-18 13:58:21 -0400

    third

---
+++ second/ensure.js
@@ -0,0 +1,5 @@
+// Implement the ensure function so that it throws an error if called without arguments or an argument is undefined. Otherwise it should return the given value.
+
+function ensure(value) {
+
+}
---
+++ second/nearHundred.js
@@ -0,0 +1,6 @@
+// Given an int n, return True if it is within 10 of 100 or 200.
+// Note: abs(num) computes the absolute value of a number.
+
+nearHundred = function(n){
+
+}
