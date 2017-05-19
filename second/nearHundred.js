28bad186d8991200fa1e971f11b9f429992eee57
Jaime Pagan <jimbopagan@yahoo.com>
2017-05-19 11:51:01 -0400

    first commit

--- second/nearHundred.js
+++ second/nearHundred.js
@@ -2,5 +2,10 @@
 // Note: abs(num) computes the absolute value of a number.
 
 nearHundred = function(n){
-
-}
+  nearHundred = function(n){
+    var total = Math.abs(100-n)  <= 10 || Math.abs(200-n) <=10;
+    if (total){
+    	return true;
+    }
+    	return false;
+  }
