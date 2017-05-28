5e26a531bfaeca93133c9d1c44c7f5aa19b8cea3
Jaime Pagan <jimbopagan@yahoo.com>
2017-05-28 16:04:40 -0400

    first commit

--- second/startHi.js
+++ second/startHi.js
@@ -2,5 +2,17 @@
 // and false otherwise.
 
 var startHi = function(str){
+  str = str.toLowerCase();
+  for(var i=0; i<str.length; i++){
+    if(str[0] === 'h' && str[1] === 'i'){
+      return true;
+    }
+  }
+  return false
+}
 
+
+var startHi = function(str){
+	var startsWithHi = /^hi/g;
+	return startsWithHi.test(str);
 }
