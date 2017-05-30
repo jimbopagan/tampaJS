125da13657fa05eabaaf8d3e84ae151b95ca35d8
Jaime Pagan <jimbopagan@yahoo.com>
2017-05-29 22:43:15 -0400

    first commit

--- second/replaceAlphabet.js
+++ second/replaceAlphabet.js
@@ -1,7 +1,19 @@
-// given a string, replace every letter with its position in the alphabet. If anything in the text isn't a letter, ignore it and don't return it. a being 1, b being 2, etc. As an example:
+// given a string, replace every letter with its position in the alphabet.
+//If anything in the text isn't a letter, ignore it and don't return it. a being 1, b being 2, etc.
+//As an example:
 //
 // alphabet_position("The sunset sets at twelve o' clock.")
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (As a string.)
 function alphabetPosition(text) {
-  return text;
+  var total =[];
+  var alphabet = "abcdefghijklmnopqrstuvwxyz";
+  for (var i =0; i< text.length; i++){
+    for(var j=0; j<alphabet.length; j++){
+      if(text[i] === alphabet[j]){
+        total.push(j + 1);
+      }
+    }
+  }
+   console.log(total);
 }
+alphabetPosition('abcdefg');
