adc8bc8a180a254783afb0d51291d76e7a8377f4
Jaime Pagan <jimbopagan@yahoo.com>
2017-06-06 14:10:37 -0400

    first commit

--- third/backAround.js
+++ third/backAround.js
@@ -2,6 +2,20 @@
 // the last char added at the front and back, so 'cat' yields 'tcatt'/
 // The original string will be length 1 or more.
 
-var backAround = function(str){ 
+var backAround = function(str){
+var lastLetter = str[str.length-1];
+// console.log(lastLetter);
+var array1 = str.split('');
+// console.log(array1);
+array1.push(lastLetter);
+array1.reverse();
+array1.push(lastLetter);
+array1.reverse();
+var newString = array1.join('');
+console.log(newString);
 
 }
+
+backAround('shoot');
+backAround('monday');
+backAround('hello');
